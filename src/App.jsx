5e744877df4132ce43ac8579826c995c665807a4
vipul <vipulmauryaedu@gmail.com>
2023-12-27 14:59:55 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
+    <Navbar/>
       <h1 className="text-3xl font-bold underline text-red-500">Vite + React + Tailwind</h1>
       <p className="read-the-docs">
         Vite and React logos to learn more
